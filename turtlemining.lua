7de410c6a23088426c3ad090e3faece556abac98
PorterOdenbach <130081149+PorterOdenbach@users.noreply.github.com>
2023-05-05 14:58:26 -0600

    link for installing

--- turtlemining.lua
+++ turtlemining.lua
@@ -1,1 +1,11 @@
-print('test')
+local scanner = peripheral.find('geoScanner')
+
+function scanBlocks(range)
+    local scan, error = scanner.scan(range)
+    if scan then
+        return scan
+    end
+    return error
+end
+
+print(textutils.serialize(scanBlocks(16)))
